ce9acbdeb93d7ed4aaaed398a4688b1c09a8207a
hilts <oscar.hilton@rvu.co.uk>
2023-06-22 22:26:28 +0000

    Styling

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 const ThreeJSAnimation = () => {
   const canvasRef = useRef(null);
@@ -12,17 +12,23 @@ const ThreeJSAnimation = () => {
       scene = new THREE.Scene();
 
       // Create a camera
-      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
       camera.position.z = 5;
 
       // Create a renderer
       renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setClearColor(0xffffff, 0);
       const canvasContainer = canvasRef.current;
       canvasContainer.appendChild(renderer.domElement);
 
       // Handle window resizing
-      window.addEventListener('resize', onWindowResize, false);
+      window.addEventListener("resize", onWindowResize, false);
 
       // Add wireframed websites
       addWireframedWebsites();
@@ -32,32 +38,40 @@ const ThreeJSAnimation = () => {
       // Wireframe material
       const material = new THREE.MeshBasicMaterial({ wireframe: true });
 
-      // Website geometry
-      const geometry = new THREE.BoxGeometry(Math.random() * 2, Math.random() * 2, Math.random() * 2);
-
       // Website 1
-      const website1 = new THREE.Mesh(geometry, material);
+      const website1 = new THREE.Mesh(createGeometry(), material);
       website1.position.x = -2;
       scene.add(website1);
 
       // Website 2
-      const website2 = new THREE.Mesh(geometry, material);
+      const website2 = new THREE.Mesh(createGeometry(), material);
       website2.position.x = 0;
       scene.add(website2);
 
       // Website 3
-      const website3 = new THREE.Mesh(geometry, material);
+      const website3 = new THREE.Mesh(createGeometry(), material);
       website3.position.x = 2;
       scene.add(website3);
+
+      function createGeometry() {
+        const width = Math.random() * 2;
+        const height = Math.random() * 2;
+        const depth = Math.random() * 2;
+
+        const geometry = new THREE.BoxGeometry(width, height, depth);
+
+        return geometry;
+      }
     };
 
     const animate = () => {
       requestAnimationFrame(animate);
 
       // Rotate websites
-      scene.children.forEach((website) => {
-        website.rotation.x += 0.01;
-        website.rotation.y += 0.01;
+      scene.children.forEach((website, i) => {
+        const rand = ((i + 58 * Math.random()) / 10000) * 2;
+        website.rotation.x += rand;
+        website.rotation.y += rand;
       });
 
       renderer.render(scene, camera);
@@ -73,11 +87,20 @@ const ThreeJSAnimation = () => {
     animate();
 
     return () => {
-      window.removeEventListener('resize', onWindowResize);
+      window.removeEventListener("resize", onWindowResize);
     };
   }, []);
 
-  return <div class="bg-black"><div style={{ opacity: 0.5 }} className="lg:fixed top-0 bottom-0 left-0 right-0" ref={canvasRef} id="canvas-container" /></div>;
+  return (
+    <div>
+      <div
+        style={{ opacity: 1 }}
+        className="lg:fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-pink-900 via-gray-900 to-gray-900"
+        ref={canvasRef}
+        id="canvas-container"
+      />
+    </div>
+  );
 };
 
 export default ThreeJSAnimation;
