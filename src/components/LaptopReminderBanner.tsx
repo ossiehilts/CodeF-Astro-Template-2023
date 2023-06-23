78c45151bb371bdc72be2abe1c68be774d1a91e3
hilts <oscar.hilton@rvu.co.uk>
2023-06-23 13:21:21 +0000

    Update LaptopReminderBanner.tsx and about.astro

--- src/components/LaptopReminderBanner.tsx
+++ src/components/LaptopReminderBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LaptopReminderBanner = ({ msg = "Remember to bring your laptop!" }) => {
   return (
-    <div className="bg-gray-900 text-cyan-300 text-center py-8 block mb-1 rounded-lg text-2xl font-light shadow-xl">
+    <div className="bg-gray-900 text-cyan-300 text-center p-8 block mb-1 rounded-lg text-2xl font-light shadow-xl">
       <p>{msg}</p>
     </div>
   );
